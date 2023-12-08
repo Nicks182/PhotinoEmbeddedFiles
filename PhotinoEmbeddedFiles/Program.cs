f9b2c71e443da63e616a2505114e49524a0d1288
Nicks182 <nic.s.bmw@gmail.com>
2023-12-08 15:19:48 +0200

    Working Example

---
+++ PhotinoEmbeddedFiles/Program.cs
@@ -0,0 +1,68 @@
+﻿using PhotinoNET;
+using System.Reflection;
+
+namespace PhotinoEmbeddedFiles
+{
+    /*
+     * Note the changes in the project file...
+        <ItemGroup>
+		    <EmbeddedResource Include="Assets\**\*" />
+	    </ItemGroup>
+     */
+
+    internal class Program
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            PhotinoWindow Win = new PhotinoWindow()
+                .RegisterCustomSchemeHandler("mycss", (object sender, string scheme, string url, out string contentType) =>
+                {
+                    contentType = "text/css";
+                    var filename = url.Replace("mycss:", "");
+                    return _GetFileStream(filename);
+                })
+                .RegisterCustomSchemeHandler("myjs", (object sender, string scheme, string url, out string contentType) =>
+                {
+                    contentType = "text/javascript";
+                    var filename = url.Replace("myjs:", "");
+                    return _GetFileStream(filename);
+                })
+
+                .RegisterWindowCreatedHandler(Win_WindowCreated)
+                .LoadRawString(_GetFileString("index.html"));
+
+            Win.WaitForClose();
+
+        }
+
+        
+
+
+        private static void Win_WindowCreated(object? sender, EventArgs e)
+        {
+            // Using this event seems to be the only way to get Maximized to work
+            // Trying to set it before the window is created seems to not work...
+            (sender as PhotinoWindow).SetMaximized(true);
+        }
+
+        const string FileDomain = "PhotinoEmbeddedFiles.Assets.";
+        private static string _GetFileString(string filename)
+        {
+            var asm = Assembly.GetExecutingAssembly();
+
+            var sr = new StreamReader(_GetFileStream(filename));
+
+            return sr.ReadToEnd();
+        }
+
+        private static Stream _GetFileStream(string filename)
+        {
+            var asm = Assembly.GetExecutingAssembly();
+
+            return asm.GetManifestResourceStream(FileDomain + filename);
+
+        }
+
+    }
+}
